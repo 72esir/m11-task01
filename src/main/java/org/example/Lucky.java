540ad7b294410839774f3078b5812d1a9d36f1b4
Aleksey Salamatin <asalamatin640@gmail.com>
2024-12-05 22:27:22 +0500

    Salamatin-m11-task01

--- src/main/java/org/example/Lucky.java
+++ src/main/java/org/example/Lucky.java
@@ -3,28 +3,21 @@ package org.example;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Lucky {
-    static int x = 0;
-    static int count = 0;
-    static final Object lock = new Object();
-    static AtomicInteger atomicInteger = new AtomicInteger(0);
-    /*static final CommonResource commonResource = new CommonResource();
-    static class CommonResource{
-        int x = 0;
-    }*/
+    static AtomicInteger atomicX = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
+    //static final Object lock = new Object();
 
     static class LuckyThread extends Thread {
-        //static class LuckyThread implements Runnable {
         @Override
         public void run() {
-            synchronized(lock){
-                while (x < 999999) {
-                    x++;
+            int x;
+            //synchronized(lock)
+            {
+                while ((x = atomicX.incrementAndGet()) < 999999) {
                     if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
                             % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
                         System.out.println(x);
-                        count++;
-                        atomicInteger.incrementAndGet();
-                        //commonResource.x++;
+                        count.incrementAndGet();
                     }
                 }
             }
@@ -32,29 +25,9 @@ public class Lucky {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        /*Runnable r = ()->{
-        while (x < 999999) {
-            x++;
-            if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                    % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                System.out.println(x);
-
-                count++;
-            }
-        }
-        };*/
-
-        /*Thread t1 = new Thread(r,"t1");
-        Thread t2 = new Thread(r,"t2");
-        Thread t3 = new Thread(r,"t3");*/
-
         Thread t1 = new LuckyThread();
         Thread t2 = new LuckyThread();
         Thread t3 = new LuckyThread();
-
-        /*Thread t1 = new Thread(new LuckyThread());
-        Thread t2 = new Thread(new LuckyThread());
-        Thread t3 = new Thread(new LuckyThread());*/
 
         t1.start();
         t2.start();
@@ -62,6 +35,7 @@ public class Lucky {
         t3.join();
         t2.join();
         t3.join();
-        System.out.println("Total: " + count + "\n" + atomicInteger);
+
+        System.out.println("Total: " + count);
     }
 }
